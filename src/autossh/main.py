eaa4ac275631375af3c3b3ad59b5defedef117e5
fabriceluo <fabriceluo@outlook.com>
2022-10-19 16:12:56 +0000

    [ADD]create refactor template

---
+++ src/autossh/main.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 fabriceluo <fabriceluo@outlook.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+def main():
+    """TODO: Docstring for main.
+    Returns: TODO
+
+    """
+    print("Hello autossh!")
+
+if __name__ == "__main__":
+    main()
